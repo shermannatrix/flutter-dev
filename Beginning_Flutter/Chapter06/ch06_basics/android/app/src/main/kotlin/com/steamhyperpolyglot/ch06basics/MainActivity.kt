fbe342f0e7aca4dc08c7e7e6c3d0079772a1574f
Sherman Chen <uxdailygeek@gmail.com>
2020-06-09 18:49:16 +0800

    Added another code example for Chapter 6

---
+++ Beginning_Flutter/Chapter06/ch06_basics/android/app/src/main/kotlin/com/steamhyperpolyglot/ch06basics/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.steamhyperpolyglot.ch06basics
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Beginning_Flutter/Chapter06/ch06_basics/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:ch06basics/pages/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'CH06 Widget Basics',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
