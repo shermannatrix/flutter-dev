7d509d710e290eaa2417e4c50647bbcfc0f27376
Sherman Chen <uxdailygeek@gmail.com>
2020-06-09 19:25:40 +0800

    Added 2 more code examples for Chapter 6

---
+++ Beginning_Flutter/Chapter06/android/app/src/main/kotlin/com/steamhyperpolyglot/Chapter06/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.steamhyperpolyglot.Chapter06
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Beginning_Flutter/Chapter06/ch06_images/android/app/src/main/kotlin/com/steamhyperpolyglot/ch06_images/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.steamhyperpolyglot.ch06_images
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
